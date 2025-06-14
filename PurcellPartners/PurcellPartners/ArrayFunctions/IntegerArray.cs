437e2109393987c5eec03133031391451be32d7f
KanikaMehhra <kanika.mehra@cenitex.vic.gov.au>
2025-06-14 14:02:08 +1000

    Adding in Integer Array class, which performs manipulations on the array.

---
+++ PurcellPartners/PurcellPartners/ArrayFunctions/IntegerArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurcellPartners
+{
+    public class IntegerArray
+    {
+        public int[] Array
+        { get; private set; }
+
+        public IntegerArray(int[] array) 
+        {
+            Array = array;
+        }
+
+        public int GetMissingNumber()
+        {
+            int minNum = Array.Min();
+            int maxNum = Array.Max();
+            var range = Enumerable.Range(minNum, maxNum - minNum + 1);
+            int rangeSum = range.Sum();
+            int actualArraySum=Array.Sum();
+            return rangeSum-actualArraySum;
+        }
+    }
+}

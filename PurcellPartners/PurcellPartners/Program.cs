eaa98055c9322a84a818d744693468ea140f6ff3
KanikaMehhra <kanika.mehra@cenitex.vic.gov.au>
2025-06-14 13:26:05 +1000

    Initial project set up

---
+++ PurcellPartners/PurcellPartners/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace PurcellPartners
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Init proj");
+        }
+    }
+}

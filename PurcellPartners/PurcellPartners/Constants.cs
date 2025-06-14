2ad6d50241a6de4c0a3c88ac2f2c8992f1a9f2fc
KanikaMehhra <kanika.mehra@cenitex.vic.gov.au>
2025-06-14 14:30:22 +1000

    updated Program with the validators

---
+++ PurcellPartners/PurcellPartners/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurcellPartners
+{
+    public class Constants
+    {
+        public const int MISSING_NUMBER_COUNT = 1;
+    }
+}
--- PurcellPartners/PurcellPartners/Program.cs
+++ PurcellPartners/PurcellPartners/Program.cs
@@ -4,13 +4,34 @@ using System.Linq;
 
 namespace PurcellPartners
 {
-    internal class Program
+    public class Program
     {
+
         static void Main(string[] args)
         {
-            IntegerArray myArray = new IntegerArray(new int[] { 0,1, 2, 4, });
-            Console.WriteLine($"Missing number is: {myArray.GetMissingNumber()}");
-            
+            IntegerArray myArray = new IntegerArray(new int[] { 0, 2, 4, });
+            IntegerArrayValidator arrayValidator = new IntegerArrayValidator();
+
+            try
+            {
+                arrayValidator.ArrayToValidate = myArray.Array;
+                arrayValidator.validateArray();
+                arrayValidator.validateNumberArray();
+                if (arrayValidator.Invaliditis.Count > 0)
+                {
+                    throw new Exception("Invalid array provided with following invalidities");
+                }
+                else
+                {
+                    Console.WriteLine($"Missing number is: {myArray.GetMissingNumber()}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                arrayValidator.Invaliditis.ForEach(inv => Console.WriteLine(inv));
+            }
+
         }
     }
 }

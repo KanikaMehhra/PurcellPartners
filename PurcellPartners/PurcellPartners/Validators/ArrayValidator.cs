dd207ef5dac9d624ec72519ff887aaf4e6938ee4
KanikaMehhra <kanika.mehra@cenitex.vic.gov.au>
2025-06-14 14:18:04 +1000

    adding validators for the array

---
+++ PurcellPartners/PurcellPartners/Validators/ArrayValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurcellPartners.Validators
+{
+    public class ArrayValidator
+    {
+        public int[] ArrayToValidate
+        { get; set; }
+        public List<string> Invaliditis
+        { get; set; } = new List<string>();
+
+        public void validateArray()
+        {
+            if(ArrayToValidate == null) {
+                Invaliditis.Add("Empty array provided");
+            }
+        }
+    }
+}
---
+++ PurcellPartners/PurcellPartners/Validators/IntegerArrayValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PurcellPartners.Validators
+{
+    public class IntegerArrayValidator:ArrayValidator
+    {
+        public void validateNumberArray()
+        {
+            var rangeLength = Enumerable.Range(ArrayToValidate.Min(), (ArrayToValidate.Max() - ArrayToValidate.Min() + 1)).Count();
+
+            //see if no number is missing
+            if (ArrayToValidate.Length== rangeLength)
+            {
+                Invaliditis.Add("No number is missing");
+            }
+
+            //see if more than one number is missing
+            if(ArrayToValidate.Length<rangeLength - Constants.MISSING_NUMBER_COUNT)
+            {
+                Invaliditis.Add("More than one number missing");
+            }
+        }
+
+    }
+}
